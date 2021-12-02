748bebc172d056551749b8021359a69c5a8de7df
dillionmegida <dillionmegida@gmail.com>
2021-12-02 10:10:23 +0100

    connecting user

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import {useState,useEffect} from 'react';
+/** @format */
 
-import {StreamChat} from 'stream-chat';
+import { useState, useEffect } from "react";
 
-const API_KEY=process.env.REACT_APP_API_KEY
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const USER1 = {
+	id: "user1",
+	name: "user1",
+	image: "https://picsum.photos/id/195/200/300",
+};
+
+const USER2 = {
+	id: "user2",
+	name: "user2",
+	image: "https://picsum.photos/id/195/200/300",
+};
+
+const USER3 = {
+	id: "user3",
+	name: "user3",
+	image: "https://picsum.photos/id/195/200/300",
+};
+
+const users = [USER1, USER2, USER3];
+
+const getRandomUser = () => {
+	const randomIndex = Math.floor(Math.random() * users.length);
+	return users[randomIndex];
+};
 
 function App() {
-    const [chatClient, setChatClient] = useState(null);
+	const [chatClient, setChatClient] = useState(null);
 
-    useEffect(() => {
-        function initChat() {
-            const client = StreamChat.getInstance(API_KEY);
-        }
+	useEffect(() => {
+		function initChat() {
+			const client = StreamChat.getInstance(API_KEY);
 
-        initChat()
-    }, [])
+			const user = getRandomUser();
 
-  return (
-    <div>
-    </div>
-  );
+			client.connectUser(user, client.devToken(user.id));
+
+			setChatClient(chatClient);
+		}
+
+		initChat();
+	}, []);
+
+	if (!chatClient) return <></>;
+
+	return (
+		<div>
+			<Chat client={chatClient} theme={"messaging light"}></Chat>
+		</div>
+	);
 }
 
 export default App;
